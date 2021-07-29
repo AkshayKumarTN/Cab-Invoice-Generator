62f133e4dc6cee1831f6825465f75cf5f8d501c7
AkshayKumarTN <tnak369@gmail.com>
2021-07-29 21:24:16 +0530

    [Akshay] Added ->Calculate Average fare for Multiple Rides

--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -28,7 +28,7 @@ namespace CabInvoiceGeneratorTest
             Assert.AreEqual(expectedFare, actualFare);
         }
         [TestMethod]
-        public void GCalculateFareMethodReturnsTotalFareMultipleRides()
+        public void CalculateFareMethodReturnsTotalFareMultipleRides()
         {
             Ride[] rides =
             {
@@ -42,6 +42,61 @@ namespace CabInvoiceGeneratorTest
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
             InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
             double actual = summary.totalFare;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void InvoiceSummaryReturnsTotalNumberOfRides()
+        {
+            // Returns Total Number Of Rides........
+            Ride[] rides =
+            {
+                new Ride(1.0, 1),
+                new Ride(2.0, 2),
+                new Ride(3.0, 2),
+                new Ride(4.0, 4),
+                new Ride(5.0, 3)
+            };
+            int expected = 5;
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            int actual = summary.numberOfRides;
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void InvoiceSummaryReturnsTotalNumberOfRidesTotalFare()
+        {
+            // Returns Total Number Of Rides Total Fare........
+            Ride[] rides =
+            {
+                new Ride(1.0, 1),
+                new Ride(2.0, 2),
+                new Ride(3.0, 2),
+                new Ride(4.0, 4),
+                new Ride(5.0, 3)
+            };
+            double expected = 162;
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            double actual = summary.totalFare;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void InvoiceSummaryReturnsTotalNumberOfRidesAverageFare()
+        {
+            // Returns Total Number Of Rides Average Fare........
+            Ride[] rides =
+            {
+                new Ride(1.0, 1),
+                new Ride(2.0, 2),
+                new Ride(3.0, 2),
+                new Ride(4.0, 4),
+                new Ride(5.0, 3)
+            };
+            double expected = 32.4;
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+            InvoiceSummary summary = invoiceGenerator.CalculateFare(rides);
+            double actual = summary.averageFare;
             Assert.AreEqual(expected, actual);
         }
     }
