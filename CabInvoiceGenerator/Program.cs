8e87861adbb59c39309c234801c8402adcf54b96
AkshayKumarTN <tnak369@gmail.com>
2021-07-29 20:25:24 +0530

    [Akshay] Added ->Calculate Total fare for the journey

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -7,6 +7,8 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome To Cab Invoice Generator");
+            InvoiceGenerator invoice = new InvoiceGenerator();
+            Console.WriteLine("\n Total fare for the journey : "+invoice.CalculateFare(2.0, 5));
         }
     }
 }
