649ccc568a41dd118623aa5cbb7e41e2a96b035d
AkshayKumarTN <tnak369@gmail.com>
2021-07-30 12:51:52 +0530

    [Akshay] Added ->Premium Rides

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -7,7 +7,7 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome To Cab Invoice Generator");
-
+            // Premium And NORMAL Ride Types....
             InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             double fare = invoiceGenerator.CalculateFare(2.0, 5);
             Console.WriteLine(" Fare : "+fare);
